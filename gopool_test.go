10f9796edbaa60ac987e31c1d88c573a12d3ce09
Daniel Hu <tao.hu@merico.dev>
2023-07-21 17:29:11 +0800

    Add unit and performance tests for GoPool
    - Added a unit test to verify the basic functionality of GoPool.
    - Added a performance test to measure the time taken by GoPool to process a million tasks with a pool size of 10000.
    - Added a performance test to measure the time taken by direct goroutines to process a million tasks.
    
    Signed-off-by: Daniel Hu <tao.hu@merico.dev>

---
+++ gopool_test.go
@@ -0,0 +1,51 @@
+package gopool
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestGoPool(t *testing.T) {
+	pool := NewGoPool(100)
+	for i := 0; i < 1000; i++ {
+		pool.AddTask(func() {
+			time.Sleep(10 * time.Millisecond)
+		})
+	}
+	pool.Release()
+}
+
+func BenchmarkGoPool(b *testing.B) {
+	var wg sync.WaitGroup
+	var taskNum = int(1e6)
+	pool := NewGoPool(10000)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		wg.Add(taskNum)
+		for num := 0; num < taskNum; num++ {
+			pool.AddTask(func() {
+				time.Sleep(10 * time.Millisecond)
+				wg.Done()
+			})
+		}
+	}
+	wg.Wait()
+	pool.Release()
+}
+
+func BenchmarkGoroutines(b *testing.B) {
+	var wg sync.WaitGroup
+	var taskNum = int(1e6)
+
+	for i := 0; i < b.N; i++ {
+		wg.Add(taskNum)
+		for num := 0; num < taskNum; num++ {
+			go func() {
+				time.Sleep(10 * time.Millisecond)
+				wg.Done()
+			}()
+		}
+	}
+}
